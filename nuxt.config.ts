1dc10e1518c4b9324c54775a76d226b7c1ee658c
marsokar <aboomarsokar@gmail.com>
2025-07-14 14:36:44 +0300

    working on SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
+  sitemap: { xsl: false },
   app: {
     head: {
       // <meta name="google-site-verification" content="us9703idIc3jQXdDeC4boohCsP9hUNsfBVWcI__Uvt0" />
